b0003ee0efedb36628870e9217ea3eeac4601879
mugambi12 <mugambi12>
2023-04-06 19:19:10 +0300

    error

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -2,23 +2,25 @@
 
 /**
  * binary_to_uint - Converts a binary number to an unsigned int.
- * @b: A pointer to a string of 0 and 1 chars.
  *
- * Return: If b is NULL or contains chars not 0 or 1 - 0.
- *         Otherwise - the converted number.
+ * @b: A string of 0 and 1 chars.
  *
- * Author: Silas Mugambi
+ * Return: The converted number, or 0 if there is one or more chars in the string
+ *         b that is not 0 or 1, or if b is NULL.
  */
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int num = 0;
 	int len = 0;
 
-	if (b[len] == '\0')
+	if (b == NULL)
 		return (0);
 
-	while ((b[len] == '0') || (b[len] == '1'))
+	while (b[len] != '\0')
 	{
+		if (b[len] != '0' && b[len] != '1')
+			return (0);
+
 		num <<= 1;
 		num += b[len] - '0';
 		len++;
