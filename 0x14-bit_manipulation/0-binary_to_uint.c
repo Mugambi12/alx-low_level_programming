11d8f199c6781fe85fb15c04a58c60edb760be5e
mugambi12 <silasmungiria16@gmail.com>
2023-04-10 15:41:17 -0700

    betty style update

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -5,8 +5,8 @@
  *
  * @b: A string of 0 and 1 chars.
  *
- * Return: The converted number, or 0 if there is one or more chars in the string
- *         b that is not 0 or 1, or if b is NULL.
+ * Return: The converted number, or 0 if there is one or more chars in the
+ *         string b that is not 0 or 1, or if b is NULL.
  */
 unsigned int binary_to_uint(const char *b)
 {
